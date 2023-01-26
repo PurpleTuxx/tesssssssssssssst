ff3e9fc46b66b56fc115c439ab64f2ce6414d44c
PurpleTuxx <122543467+PurpleTuxx@users.noreply.github.com>
2023-01-26 09:50:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,2 @@
 OLED.init(128, 64)
-OLED.writeStringNewLine("Hello student.  The world is ending.  This message will self destruct. 5... 4... 3.... 2... 1...")
+OLED.writeStringNewLine("Hello student.  This world is ending.  This message is going to self destruct. 5... 4... 3... 2... 1...")
